536cc20bdff1e3545bce9e5810eb866baf83dc9e
nadal28 <nadal_28@hotmail.es>
2016-11-29 11:52:42 +0100

    Create curl.php

---
+++ curl/curl.php
@@ -0,0 +1,30 @@
+<?php
+
+function curl($url, $args) {
+
+	$args = implode('&', array_map(
+		function ($v, $k) { return sprintf("%s=%s", $k, $v); },
+		$args,
+		array_keys($args)
+		));
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  
+  if(!empty($args){
+    curl_setopt($ch, CURLOPT_POST, 1);
+	  curl_setopt($ch, CURLOPT_POSTFIELDS, $args);
+  }  
+	
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	return $response;
+}
+
+?>
