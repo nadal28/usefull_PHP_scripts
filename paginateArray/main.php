5e4972b0d17916b8fc009a979a20a9d10f23881c
nadal28 <nadal28@users.noreply.github.com>
2017-08-02 08:39:57 +0200

    Update main.php

--- paginateArray/main.php
+++ paginateArray/main.php
@@ -5,7 +5,7 @@ function paginateArray($array, $page, $itemsPerPage) {
 	$totalPages = ceil(count($array) / $itemsPerPage);
 
 	$page = $page > $totalPages ? $totalPages : $page;	//If the page requested is too higher then we show the highest available page
-	$page = $page < 1 ? 1 : $page;						//If the requested page is below 1 then we show the first page
+	$page = $page < 1 ? 1 : $page;				//If the requested page is below 1 then we show the first page
 	$start = ($page - 1) * $itemsPerPage;
 
 	return array_slice($array, $start, $itemsPerPage);
